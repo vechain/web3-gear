e300392735a5f13cde69402d5289c0e96c2ffa7d
hanxiao <zhuec@hotmail.com>
2018-07-11 13:35:04 +0800

    refactor: request
    重构 restful 为链式调用

--- gear/thor/client.py
+++ gear/thor/client.py
@@ -15,7 +15,7 @@ from gear.utils.compat import (
     intrinsic_gas,
 )
 from .request import (
-    make_request,
+    Restful,
     get,
     post,
 )
@@ -23,21 +23,14 @@ from .request import (
 
 class ThorClient(object, metaclass=Singleton):
     def __init__(self):
-        self.account = lambda address: make_request("{0}/accounts/{1}".format(self.endpoint, address))\
-            if address else\
-            make_request("{0}/accounts".format(self.endpoint))
-        self.transaction = lambda id: make_request("{0}/transactions/{1}".format(self.endpoint, id)) \
-            if id else \
-            make_request("{0}/transactions".format(self.endpoint))
-        self.receipt = lambda id: make_request("{0}/transactions/{1}/receipt".format(self.endpoint, id))
-        self.code = lambda address: make_request("{0}/accounts/{1}/code".format(self.endpoint, address))
-        self.trace = lambda id: make_request("{0}/transactions/{1}/trace".format(self.endpoint, id))
-        self.block = lambda revision: make_request("{0}/blocks/{1}".format(self.endpoint, revision))
-        self.events = lambda: make_request("{0}/events".format(self.endpoint))
         self.filter = {}
 
     def set_endpoint(self, endpoint):
-        self.endpoint = endpoint
+        restful = Restful(endpoint)
+        self.transactions = restful.transactions
+        self.blocks = restful.blocks
+        self.accounts = restful.accounts
+        self.events = restful.events
 
     def set_accounts(self, account_manager):
         self.account_manager = account_manager
@@ -48,23 +41,20 @@ class ThorClient(object, metaclass=Singleton):
         data = {
             "logConfig": params,
         }
-        return self.trace(tx_hash)(post, data=data)
+        return self.transactions(tx_hash).trace.make_request(post, data=data)
 
     def storage_range_at(self, blk_hash, tx_index, contract_addr, key_start):
-        '''
-        TODO
-        '''
         raise Exception("Did not implement this interface.")
 
     def get_accounts(self):
         return self.account_manager.get_accounts()
 
     def get_block_number(self):
-        block = self.block("best")(get)
+        block = self.blocks("best").make_request(get)
         return None if block is None else block["number"]
 
     def get_block_id(self, block_identifier):
-        blk = self.block(block_identifier)(get)
+        blk = self.blocks(block_identifier).make_request(get)
         return None if blk is None else blk["id"]
 
     def estimate_gas(self, transaction):
@@ -77,7 +67,7 @@ class ThorClient(object, metaclass=Singleton):
             "value": (encode_number(transaction.get("value", 0))).decode("utf-8"),
             "caller": transaction["from"],
         }
-        result = self.account(to_addr)(post, data=data)
+        result = self.accounts(to_addr).make_request(post, data=data)
         if result is None:
             return encode_number(0)
         return encode_number(int(result["gasUsed"] * 1.2) + intrinsic_gas(transaction))
@@ -90,7 +80,7 @@ class ThorClient(object, metaclass=Singleton):
             "data": transaction["data"],
             "value": (encode_number(transaction.get("value", 0))).decode("utf-8"),
         }
-        result = self.account(transaction.get("to", None))(post, data=data, params=params)
+        result = self.accounts(transaction.get("to", None)).make_request(post, data=data, params=params)
         return None if result is None else result["data"]
 
     def send_transaction(self, transaction):
@@ -99,33 +89,33 @@ class ThorClient(object, metaclass=Singleton):
         data = {
             "raw": "0x{}".format(encode_hex(rlp.encode(tx)))
         }
-        result = self.transaction(None)(post, data=data)
+        result = self.transactions.make_request(post, data=data)
         return None if result is None else result["id"]
 
     def get_transaction_by_hash(self, tx_hash):
-        tx = self.transaction(tx_hash)(get)
+        tx = self.transactions(tx_hash).make_request(get)
         return None if tx is None else thor_tx_convert_to_eth_tx(tx)
 
     def get_balance(self, address, block_identifier):
         params = {
             "revision": block_identifier
         }
-        accout = self.account(address)(get, params=params)
+        accout = self.accounts(address).make_request(get, params=params)
         return None if accout is None else accout["balance"]
 
     def get_transaction_receipt(self, tx_hash):
-        receipt = self.receipt(tx_hash)(get)
+        receipt = self.transactions(tx_hash).receipt.make_request(get)
         return None if receipt is None else thor_receipt_convert_to_eth_receipt(receipt)
 
     def get_block(self, block_identifier):
-        blk = self.block(block_identifier)(get)
+        blk = self.blocks(block_identifier).make_request(get)
         return None if blk is None else thor_block_convert_to_eth_block(blk)
 
     def get_code(self, address, block_identifier):
         params = {
             "revision": block_identifier
         }
-        code = self.code(address)(get, params=params)
+        code = self.accounts(address).code.make_request(get, params=params)
         return None if code is None else code["code"]
 
     def new_block_filter(self):
@@ -146,7 +136,7 @@ class ThorClient(object, metaclass=Singleton):
         params = {
             "address": address
         }
-        logs = self.events()(post, data=query, params=params)
+        logs = self.events.make_request(post, data=query, params=params)
         result = thor_log_convert_to_eth_log(address, logs)
         return result
 
