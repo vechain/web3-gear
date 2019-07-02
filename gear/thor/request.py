f1d7c8515cece96603d4998e9e4184d44113fa02
hanxiao <zhuec@hotmail.com>
2019-07-02 10:46:42 +0800

    refactor: asynchronous request

--- gear/thor/request.py
+++ gear/thor/request.py
@@ -1,77 +1,14 @@
-import lru
-import json
-import requests
-import hashlib
-from eth_utils import (
-    is_boolean,
-    is_bytes,
-    is_dict,
-    is_list_like,
-    is_null,
-    is_number,
-    is_text,
-    to_bytes,
-)
-from jsonrpc.exceptions import JSONRPCDispatchException
-from collections import Generator
+import aiohttp
 
 
-def _generate_cache_key(value):
-    '''
-    Generates a cache key for the *args and **kwargs
-    '''
-    if is_bytes(value):
-        return hashlib.md5(value).hexdigest()
-    elif is_text(value):
-        return _generate_cache_key(to_bytes(text=value))
-    elif is_boolean(value) or is_null(value) or is_number(value):
-        return _generate_cache_key(repr(value))
-    elif is_dict(value):
-        return _generate_cache_key((
-            (key, value[key])
-            for key
-            in sorted(value.keys())
-        ))
-    elif is_list_like(value) or isinstance(value, Generator):
-        return _generate_cache_key("".join((
-            _generate_cache_key(item)
-            for item
-            in value
-        )))
-    else:
-        raise TypeError("Cannot generate cache key for value {0} of type {1}".format(
-            value,
-            type(value),
-        ))
+async def post(endpoint_uri, data, **kwargs):
+    async with aiohttp.ClientSession() as session:
+        return await session.post(endpoint_uri, json=data, **kwargs)
 
 
-def _remove_session(key, session):
-    session.close()
-
-
-_session_cache = lru.LRU(8, callback=_remove_session)
-
-
-def _get_session(*args, **kwargs):
-    '''
-    request.Session Provides cookie persistence, connection-pooling, and configuration.
-    '''
-    cache_key = _generate_cache_key((args, kwargs))
-    if cache_key not in _session_cache:
-        _session_cache[cache_key] = requests.Session()
-    return _session_cache[cache_key]
-
-
-def post(endpoint_uri, data, **kwargs):
-    session = _get_session(endpoint_uri)
-    response = session.post(endpoint_uri, json=data, **kwargs)
-    return response
-
-
-def get(endpoint_uri, params, **kwargs):
-    session = _get_session(endpoint_uri)
-    response = session.get(endpoint_uri, params=params, **kwargs)
-    return response
+async def get(endpoint_uri, params, **kwargs):
+    async with aiohttp.ClientSession() as session:
+        return await session.get(endpoint_uri, params=params, **kwargs)
 
 
 class Restful(object):
@@ -88,7 +25,7 @@ class Restful(object):
     def __getattr__(self, resource):
         return Restful('%s/%s' % (self._endpoint, resource))
 
-    def make_request(self, method, params=None, data=None, **kwargs):
+    async def make_request(self, method, params=None, data=None, **kwargs):
         headers = {
             "accept": "application/json",
             "Connection": "keep-alive",
@@ -97,13 +34,15 @@ class Restful(object):
         kwargs.setdefault('headers', headers)
         kwargs.setdefault('timeout', 10)
         try:
-            response = method(self._endpoint, params=params, data=data, **kwargs)
+            response = await method(self._endpoint, params=params, data=data, **kwargs)
             response.raise_for_status()
-            return json.loads(response.content)
-        except requests.exceptions.ConnectionError:
-            print("Unable to connect to Thor-Restful server.")
-        except requests.RequestException as e:
-            print("Thor-Restful server Err:", e)
+            return await response.json()
+        except aiohttp.ClientConnectionError as e:
+            print("Unable to connect to Thor-Restful server:")
+            print(e)
+        except Exception as e:
+            print("Thor-Restful server Err:")
+            print(e)
             print(response.content)
-            raise JSONRPCDispatchException(-32000, message=response.content.decode().strip('\n'))
+            raise ValueError(response.content.decode().strip('\n'))
         return None
