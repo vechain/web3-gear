63ea06beaba62b9171b13122781b795287e288f8
hanxiao <zhuec@hotmail.com>
2019-08-20 18:58:15 +0800

    release: v2.0.1

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open(path.join(here, "README.md"), encoding="utf-8") as f:
 
 setup(
     name="web3-gear",
-    version="2.0.0",
+    version="2.0.1",
     description="An adapter between thor-restful and eth-rpc.",
     long_description=long_description,
     long_description_content_type="text/markdown",
